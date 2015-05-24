c8f1f9ef2b5665d918905f30f6639401b9f66bdc
sunpascal <sunpascal@gmail.com>
2015-05-25 00:04:55 +0200

    drawer backup

--- src/de/unibamberg/eesys/projekt/gui/MainActivity.java
+++ src/de/unibamberg/eesys/projekt/gui/MainActivity.java
@@ -1,6 +1,8 @@
 package de.unibamberg.eesys.projekt.gui;
 
 import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.AlertDialog;
 import android.app.FragmentTransaction;
 import android.content.DialogInterface;
@@ -37,7 +39,7 @@ import de.unibamberg.eesys.projekt.R;
  * @author Julia
  *
  */
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements TabListener{
 
 	static final String TAG = "MainActivity";
 	static final String WAYPOINT = "waypoint";
@@ -53,14 +55,216 @@ public class MainActivity extends FragmentActivity {
 	private boolean gpsEnabled;
 	private boolean open;
 
-	private String shownFragment = "";
+	private MODE shownFragment = MODE.DASHBOARD;
 
 	DashboardFragment gpsUpdateListener;
 	
 	// tabs 
 	private DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
 	private ViewPager mViewPager;
-	private int numberOfTabs = 8; 	
+	private ActionBar actionBar; 
+	
+	private static enum MODE {
+		DASHBOARD, EV_RECOMMENDATION, ECO_DRIVING, ANALYSIS, BLANK
+	};
+	
+	/**
+	 * Manages the options from the drawer and inflates the selected Fragment
+	 * 
+	 * @param position of the option in the drawer
+	 */
+	public void selectItem(int position) {
+
+		// check that the activity is using the layout version with the
+		// content_frame FrameLayout
+		if (this.findViewById(R.id.content_frame) != null) {
+
+			// create a new Fragment to be placed in the activity layout
+			Fragment selectedFragment = null;
+			switch (position) {
+			
+			case 0:
+				selectedFragment = new DashboardFragment();
+				shownFragment = MODE.DASHBOARD;
+				actionBar.removeAllTabs();
+//				actionBar.hide();
+				break;	
+			case 1:			
+				selectedFragment = new EvRecommendationFragment();
+				shownFragment = MODE.EV_RECOMMENDATION;
+		    	actionBar.removeAllTabs();
+//		    	actionBar.hide();
+				break;		
+			case 2:
+				selectedFragment = new EcoDrivingFragment();
+				shownFragment = MODE.ECO_DRIVING;
+				break;	
+			case 3:
+				selectedFragment = new AnalysisFragment();
+				shownFragment = MODE.ANALYSIS;
+				break;		
+				
+			default: 
+					selectedFragment = new DashboardFragment();
+					shownFragment = MODE.DASHBOARD;
+					actionBar.removeAllTabs();
+//					actionBar.hide();
+				break;
+			}
+			
+//				showBlankFragment();
+				
+				// in case this activity was started with special instructions from
+				// an Intent, pass the Intent's extras to the fragment as arguments
+				selectedFragment.setArguments(this.getIntent().getExtras());
+				Bundle args = new Bundle();
+				args.putInt(selectedFragment.toString(), position);
+				selectedFragment.setArguments(args);
+	
+				// add the fragment to the 'content_frame' FrameLayout
+				getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, selectedFragment).commit();
+				getSupportFragmentManager().executePendingTransactions();
+	
+				// update selected item and title, then close the drawer
+				this.mDrawerList.setItemChecked(position, true);
+				this.setTitle(this.mDrawerOptions[position]);
+				this.mDrawerLayout.closeDrawer(mDrawerList);
+				
+				// hier tabs für entsprechendes Drawer menü vorbereiten
+				if (shownFragment == MODE.ANALYSIS) {
+				
+					// Add n tabs, specifying the tab's text and TabListener
+				    for (int i = 0; i < 2; i++) {
+				    	actionBar.addTab(
+				                actionBar.newTab()
+				                        .setText("" + (i + 1))
+				                        .setTabListener(this));
+				    }
+				    actionBar.show();				
+				}
+		}
+	}
+
+
+	// Since this is an object collection, use a FragmentStatePagerAdapter,
+	// and NOT a FragmentPagerAdapter.
+	public class DemoCollectionPagerAdapter extends FragmentStatePagerAdapter {
+	    public DemoCollectionPagerAdapter(FragmentManager fm) {
+	        super(fm);
+	    }
+
+	    @Override
+	    /** open tab */ 
+	    public Fragment getItem(int i) {
+	    	
+	    	Fragment fragment =  null;
+	    	
+	    	// todo: hier unterscheiden welches Drawer menü offen ist!
+	    	if (shownFragment == MODE.ANALYSIS) {
+	    	
+		    	switch (i) {
+		    	
+		    		case 0: {
+		    			fragment =  new DashboardFragment();
+		    			break; 
+		    		}	   	    	
+		    	
+		    		case 7: {
+			    		fragment =  new EcoDrivingFeedbackTeqniqueFragment();
+			    		break; 
+		    		}	    	
+		    	
+		    		case 1: {
+		    			fragment = new DriversLogFragment();
+		    			break; 
+		    		}
+		    		case 2: {
+		    			fragment = new StateOfChargeFragment();
+		    			break; 
+		    		}	  
+		    		case 3: {
+		    			fragment = new DrivePossibilityFragment();
+		    			break; 
+		    		}	   
+		    		case 4: {
+		    			fragment = new DriveGraphFragment();
+		    			break; 
+		    		}	 	
+		    		case 5: {
+		    			fragment = new DriveDistancesFragment();
+		    			break; 
+		    		}		 
+		    		case 6: {
+		    			fragment = new AvgConsumptionFragment();
+		    			break; 
+		    		}	
+		    	}
+	    		
+	    		
+	    	}
+	    	
+//	    	showBlankFragment();
+	    	
+	    	if (fragment != null) { 
+	    		Bundle args = new Bundle();
+	    		args.putString("tabName", fragment.toString());
+	    		fragment.setArguments(args);
+	    	}
+	        return fragment;
+	    }
+	    
+
+	    @Override
+	    public int getCount() {
+	        return 100;
+	    }
+
+	    @Override
+	    public CharSequence getPageTitle(int position) {
+	        return "OBJECT " + (position + 1);
+	    }
+
+	}
+
+	public final void showBlankFragment() {
+			Fragment selectedFragment = new BlankFragment();
+			selectedFragment.setArguments(this.getIntent().getExtras());
+			Bundle args = new Bundle();
+			args.putInt(selectedFragment.toString(), 0);
+			selectedFragment.setArguments(args);
+
+			this.getSupportFragmentManager().beginTransaction()
+					.replace(R.id.content_frame, selectedFragment).commit();
+			shownFragment = MODE.BLANK;
+	}	
+	
+	@Override
+	/** 
+	 *  When swiping between pages, select the corresponding tab.
+	 *  When the tab is selected, switch to the corresponding page in the ViewPager.
+	 */
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        
+	    	if ( tab != null) {
+	    		
+	    		// take current drawer into account: 
+	    		if (shownFragment == MODE.ANALYSIS) {
+	    			mViewPager.setCurrentItem(tab.getPosition());
+	    		}
+	    	}
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}	
 	
 	
 	/**
@@ -88,10 +292,12 @@ public class MainActivity extends FragmentActivity {
 		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
 				R.layout.drawer_list_item, mDrawerOptions));
 		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+		
+		actionBar = getActionBar(); 
 
 		// enable ActionBar app icon to behave as action to toggle nav drawer
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-		getActionBar().setHomeButtonEnabled(true);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setHomeButtonEnabled(true);
 
 		// ActionBarDrawerToggle ties together the the proper interactions
 		// between the sliding drawer and the action bar app icon
@@ -102,13 +308,13 @@ public class MainActivity extends FragmentActivity {
 		R.string.drawer_close /* "close drawer" description for accessibility */
 		) {
 			public void onDrawerClosed(View view) {
-				getActionBar().setTitle(mTitle);
+				actionBar.setTitle(mTitle);
 				invalidateOptionsMenu(); // creates call to
 											// onPrepareOptionsMenu()
 			}
 
 			public void onDrawerOpened(View drawerView) {
-				getActionBar().setTitle(mDrawerTitle);
+				actionBar.setTitle(mDrawerTitle);
 				invalidateOptionsMenu(); // creates call to
 											// onPrepareOptionsMenu()
 			}
@@ -141,7 +347,6 @@ public class MainActivity extends FragmentActivity {
 		// Action bar / tabs 
 		
 	    mViewPager = (ViewPager) findViewById(R.id.viewPager);
-	    final ActionBar actionBar = getActionBar();
 
 	    // Specify that tabs should be displayed in the action bar.
 	    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -153,106 +358,26 @@ public class MainActivity extends FragmentActivity {
                 		getSupportFragmentManager());
         mViewPager.setAdapter(mDemoCollectionPagerAdapter);
 	    
-	    
-	    // Create a tab listener that is called when the user changes tabs.
-	    ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-	        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-	            // When the tab is selected, switch to the
-	            // corresponding page in the ViewPager.
-	        	
-//            	 int t =getActivity().getActionBar().getTabCount();
-                // When swiping between pages, select the corresponding tab.
-            	
-            	ViewPager vp = mViewPager;
-            	if ( vp != null && tab != null) {     	
-            		mViewPager.setCurrentItem(tab.getPosition());
-            	}
-	        }
-
-	        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-	            // hide the given tab
-	        }
-
-	        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-	            // probably ignore this event
-	        }
-	    };
-
-	    
-    	// todo: figure out how to manage tab lifecycle
-    	actionBar.removeAllTabs();
-	    
-	    // Add n tabs, specifying the tab's text and TabListener
-	    for (int i = 0; i < numberOfTabs; i++) {
-	        
-	    	actionBar.addTab(
-	                actionBar.newTab()
-	                        .setText("" + (i + 1))
-	                        .setTabListener(tabListener));
-	    }
-	    
-	    
+	   	    
 	    // respond to user swipes
 	    mViewPager.setOnPageChangeListener(
 	            new ViewPager.SimpleOnPageChangeListener() {
 	                @Override
 	                public void onPageSelected(int position) {
 	                    // When swiping between pages, select the corresponding tab.
-	                	if (position < getActionBar().getTabCount()-1) {
-	                		getActionBar().setSelectedNavigationItem(position);
+	                	if (shownFragment == MODE.ANALYSIS) {	                	
+	                		if (position < actionBar.getTabCount()-1) {
+	                			actionBar.setSelectedNavigationItem(position);
+	                		}
 	                	}
 	                }
 	            });	    
 	    
 	    getSupportFragmentManager().executePendingTransactions();
-	    
 		
 	}
 
-	/**
-	 * Displays alert dialog if GPS is not enabled when app is started or
-	 * resumed.
-	 * 
-	 */
-	public void gpsAlert() {
-		appContext = (AppContext) getApplicationContext();
-		gpsEnabled = appContext.isGpsEnabled();
-		if (gpsEnabled == false && open == false) {
-			open = true;
-			final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage(
-					"Your GPS seems to be disabled, do you want to enable it?")
-					.setCancelable(true)
-					.setPositiveButton("Yes",
-							new DialogInterface.OnClickListener() {
-								public void onClick(
-										final DialogInterface dialog,
-										final int id) {
-									startActivity(new Intent(
-											Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-									dialog.cancel();
-									open = false;
-								}
-							})
-					.setNegativeButton("No",
-							new DialogInterface.OnClickListener() {
-								public void onClick(
-										final DialogInterface dialog,
-										final int id) {
-									dialog.cancel();
-									open = false;
-								}
-							});
-			final AlertDialog alert = builder.create();
-			try {
-				alert.show();
-			} catch (Exception e) {
-				Log.d(TAG, "Bad Token Exception");
-			}
-
-		}
-	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -299,101 +424,13 @@ public class MainActivity extends FragmentActivity {
 		}
 	}
 
-	/**
-	 * Manages the options from the drawer and inflates the selected Fragment
-	 * 
-	 * @param position of the option in the drawer
-	 */
-	public void selectItem(int position) {
-
-		// check that the activity is using the layout version with the
-		// content_frame FrameLayout
-		if (this.findViewById(R.id.content_frame) != null) {
-
-			// create a new Fragment to be placed in the activity layout
-			Fragment selectedFragment = null;
-			switch (position) {
-			
-			case 0:
-				selectedFragment = new BlankFragment();
-				shownFragment = "BlankShown";
-				break;				
-			case 1:
-				selectedFragment = new DashboardFragment();
-				shownFragment = "DashboardShown";
-				break;	
-			case 2:			
-				selectedFragment = new EvRecommendationFragment();
-				shownFragment = "EvRecommendationShown";
-				break;		
-			case 3:
-				selectedFragment = new EcoDrivingFragment();
-				shownFragment = "EcoDrivingShown";
-				break;	
-			case 4:
-				selectedFragment = new AnalysisFragment();
-				shownFragment = "AnalysisShown";
-				break;						
-			
-			/* Menüeinträge aus Projekt App */ 
-			/*
-			case 1:
-				selectedFragment = new StateOfChargeFragment();
-				shownFragment = "StateOfChargeShown";
-				break;
-			case 2:
-				selectedFragment = new AvgConsumptionFragment();
-				shownFragment = "AvgConsumptionShown";
-				break;
-			case 3:
-				selectedFragment = new DrivePossibilityFragment();
-				shownFragment = "DrivePossibilityShown";
-				break;
-			case 4:
-				selectedFragment = new DriveDistancesFragment();
-				shownFragment = "driveDistanceShown";
-				break;
-			case 5:
-				selectedFragment = new DriversLogFragment();
-				shownFragment = "DriversLogShown";
-				break;
-			case 6:
-				selectedFragment = new DriveGraphFragment();
-				shownFragment = "DriveGraphShown";
-				break;		
-			*/	
-				
-			}
-			
-			if (selectedFragment != null) {
-				
-				// in case this activity was started with special instructions from
-				// an Intent, pass the Intent's extras to the fragment as arguments
-				selectedFragment.setArguments(this.getIntent().getExtras());
-				Bundle args = new Bundle();
-				args.putInt(selectedFragment.toString(), position);
-	
-				selectedFragment.setArguments(args);
-	
-				// add the fragment to the 'content_frame' FrameLayout
-				this.getSupportFragmentManager().beginTransaction()
-						.replace(R.id.content_frame, selectedFragment).commit();
-				
-				getSupportFragmentManager().executePendingTransactions();
-	
-				// update selected item and title, then close the drawer
-				this.mDrawerList.setItemChecked(position, true);
-				this.setTitle(this.mDrawerOptions[position]);
-				this.mDrawerLayout.closeDrawer(mDrawerList);
-			}
-		}
-	}
 
 	@Override
 	/**
 	 * Handles the back button of Android phones 
 	 * Used to handle back navigation between the fragments when selecting out of the drawer menu 
 	 * Finishes the app after back button is pressed in the main overview
+	 * ToDo: also select Dashboard drawer!!
 	 */
 	public final void onBackPressed() {
 		if (shownFragment.equals("BlankShown")) {
@@ -410,21 +447,10 @@ public class MainActivity extends FragmentActivity {
 
 			this.getSupportFragmentManager().beginTransaction()
 					.replace(R.id.content_frame, selectedFragment).commit();
-			shownFragment = "StatusShown";
+			shownFragment = MODE.DASHBOARD;
 		}
 	}
 	
-	public final void showBlankFragment() {
-			Fragment selectedFragment = new BlankFragment();
-			selectedFragment.setArguments(this.getIntent().getExtras());
-			Bundle args = new Bundle();
-			args.putInt(selectedFragment.toString(), 0);
-			selectedFragment.setArguments(args);
-
-			this.getSupportFragmentManager().beginTransaction()
-					.replace(R.id.content_frame, selectedFragment).commit();
-			shownFragment = "BlankShown";
-	}	
 
 	@Override
 	public void setTitle(CharSequence title) {
@@ -464,76 +490,52 @@ public class MainActivity extends FragmentActivity {
 		appContext.updateGui();
 	}
 	
-	// Since this is an object collection, use a FragmentStatePagerAdapter,
-	// and NOT a FragmentPagerAdapter.
-	public class DemoCollectionPagerAdapter extends FragmentStatePagerAdapter {
-	    public DemoCollectionPagerAdapter(FragmentManager fm) {
-	        super(fm);
-	    }
 
-	    @Override
-	    public Fragment getItem(int i) {
-	    	
-	    	Fragment fragment =  null;
-	    	
-	    	switch (i) {
-	    	
-	    		case 0: {
-	    		fragment =  new BlankFragment();
-	    		break; 
-	    		}		    	
-	    	
-	    		case 1: {
-	    		fragment =  new EcoDrivingFeedbackTeqniqueFragment();
-	    		break; 
-	    		}	    	
-	    	
-	    		case 2: {
-	    			fragment = new DriversLogFragment();
-	    			break; 
-	    		}
-	    		case 3: {
-	    			fragment = new StateOfChargeFragment();
-	    			break; 
-	    		}	  
-	    		case 4: {
-	    			fragment = new DrivePossibilityFragment();
-	    			break; 
-	    		}	   
-	    		case 5: {
-	    			fragment = new DriveGraphFragment();
-	    			break; 
-	    		}	 	
-	    		case 6: {
-	    			fragment = new DriveDistancesFragment();
-	    			break; 
-	    		}		 
-	    		case 8: {
-	    			fragment = new AvgConsumptionFragment();
-	    			break; 
-	    		}		    		
-	    	}
-	    	
-	    	showBlankFragment();
-	    	
-	        Bundle args = new Bundle();
-	        // Our object is just an integer :-P
-	        args.putInt(EcoDrivingBatteryStateFragment.ARG_OBJECT, i + 1);
-	        fragment.setArguments(args);
-	        return fragment;
-	    }
-
-	    @Override
-	    public int getCount() {
-	        return 100;
-	    }
-
-	    @Override
-	    public CharSequence getPageTitle(int position) {
-	        return "OBJECT " + (position + 1);
-	    }
-
-	}
 	
+	
+	/**
+	 * Displays alert dialog if GPS is not enabled when app is started or
+	 * resumed.
+	 * 
+	 */
+	public void gpsAlert() {
+		appContext = (AppContext) getApplicationContext();
+		gpsEnabled = appContext.isGpsEnabled();
+		if (gpsEnabled == false && open == false) {
+			open = true;
+			final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(
+					"Your GPS seems to be disabled, do you want to enable it?")
+					.setCancelable(true)
+					.setPositiveButton("Yes",
+							new DialogInterface.OnClickListener() {
+								public void onClick(
+										final DialogInterface dialog,
+										final int id) {
+									startActivity(new Intent(
+											Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+									dialog.cancel();
+									open = false;
+								}
+							})
+					.setNegativeButton("No",
+							new DialogInterface.OnClickListener() {
+								public void onClick(
+										final DialogInterface dialog,
+										final int id) {
+									dialog.cancel();
+									open = false;
+								}
+							});
+			final AlertDialog alert = builder.create();
+			try {
+				alert.show();
+			} catch (Exception e) {
+				Log.d(TAG, "Bad Token Exception");
+			}
 
+		}
+	}
+
+	
 }
